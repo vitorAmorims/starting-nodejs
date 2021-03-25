99e63a0bc03ed39471ac526027b649f485d25996
vitorAmorims <vtamorims@gmail.com>
2021-03-25 09:43:58 -0300

    node.js - starting with fibonacci sequence

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+
 const readline = require('readline-sync');
 
-function calcularFatorial () {
-    let n = readline.questionInt("Deseja fatorial de qual número?\n");
-    let numero = n;
-    let fatorial  = 1;
-    while (n > 0) {
-        fatorial *= n;
-        n -= 1;
-    }
-    return console.log(`\nO fatorial de ${numero} é igual ${fatorial}`)   
+function sequenciaFibonacci() {
+  console.log('Calcular sequência de Fibonacci\n')
+  let numero = readline.questionInt('\nDigite o número: ')
+  let anterior = 0;
+  let atual = 1;
+  let proximo = 1;
+  let contador = 1;
+  let total = 0
+  while(contador <= numero) {
+    console.log(proximo);
+    total += proximo;
+    proximo = atual + anterior;
+    anterior = atual
+    atual = proximo;
+    contador++;
+  }
+  return console.log(` \nO valor total da sequência é: ${total}`)
 }
 
-calcularFatorial()
+sequenciaFibonacci()
